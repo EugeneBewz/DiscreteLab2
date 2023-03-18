2627767ed804190cec4afb2f3ffee1bf17d0057d
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-03-18 22:36:26 +0200

    lz77: jupyther in progress

--- LZ77/lz77.py
+++ LZ77/lz77.py
@@ -5,6 +5,17 @@ LZ77 compression
 from typing import List, Tuple
 
 
+def read_file(filename: str) -> str:
+    """
+    Read file and return a solid string with data.
+
+    :param filename: path to file
+    :return: string with text
+    """
+    with open(filename, 'r') as file:
+        return file.read()
+
+
 class LZ77:
     def __init__(self, buffer_size: int = 5, box_size: int = 5):
         self.buffer_size = buffer_size
@@ -31,7 +42,7 @@ class LZ77:
             buffer = message[buffer_begin:buffer_end]
             box = message[box_begin:cursor]
 
-            for i in range(len(buffer) - 1):
+            for i in range(len(buffer)):
                 j = box.rfind(buffer[:i + 1])
                 if j != -1:
                     pattern = (cursor - box_begin - j, i + 1, buffer[i + 1])
@@ -70,7 +81,7 @@ class LZ77:
 
 
 if __name__ == "__main__":
-    lz77 = LZ77()
+    lz77 = LZ77(5, 10)
     compressed_message = lz77.compress("abacabacdbacacacd")
     print(compressed_message)
     decompressed_message = lz77.decompress(compressed_message)
